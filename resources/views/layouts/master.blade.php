ef0766fca0b4efa5e6d66ebb23d5fb8dd76b4e74
SNishalaka <s.nishalaka97@gmail.com>
2019-07-27 00:17:56 +0530

    Add new master blade view and modified default blade view file(welcome.blade.php)

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        
+    </head>
+    <body>
+
+        @section('sidebar')
+            <!-- This is the master sidebar -->
+        @show
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>  
